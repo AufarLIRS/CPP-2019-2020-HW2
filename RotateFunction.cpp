272247f3baba53d393d8325873f27d3acdf8e87e
waleramir <42164244+waleramir@users.noreply.github.com>
2019-10-28 22:46:53 +0300

    rotation fix

--- RotateFunction.cpp
+++ RotateFunction.cpp
@@ -1,5 +1,5 @@
 void rotate(int *first, int *last) {
-  for (; first != last; first++, last--) {
+  for (; first < last; first++, last--) {
     auto temp = *first;
     *first = *last;
     *last = temp;
