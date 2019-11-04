0e4257725e46478589c39bbd47ab1837a0d1bd5a
NotArtyom <smart.artyom@gmail.com>
2019-11-05 01:31:46 +0300

    Task 1 & 2

--- main.cpp
+++ main.cpp
@@ -2,8 +2,31 @@
 
 using namespace std;
 
+int min_element (int* m, int size){
+    int min = m[0];
+    for (int i = 0; i < size; i++){
+        if(min > m[i]){
+            min = m[i];
+        }
+    }
+    return min;
+}
+
+int min_element (int* first, int* last){
+    int min = *first;
+    for (;first != last; ++first){
+        if (*first < min)
+            min = *first;
+    }
+    return min;
+
+}
+
+void rotate (int* first, int* last){
+
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+
 }
